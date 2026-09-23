64272e83bc85773ac4a50ee3d7a28ca1aceae0b2
Palak Dhussa <dhussapalak@gmail.com>
2026-09-23 22:36:49 +0530

    even_odd_checker.c

---
+++ even_odd_checker.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int number;
+
+    printf("===== EVEN OR ODD CHECKER =====\n");
+
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0) {
+        printf("%d is an EVEN number.\n", number);
+    } else {
+        printf("%d is an ODD number.\n", number);
+    }
+
+    return 0;
+}
